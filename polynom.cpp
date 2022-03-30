f90c9dc9cddb1efcfabac3b48eac01748e097cc9
vonkka <96284529+vonkka@users.noreply.github.com>
2022-03-31 00:43:31 +0300

    Update polynom.cpp

--- polynom.cpp
+++ polynom.cpp
@@ -1,288 +1,218 @@
-
 #include <stdlib.h>
 #include <iostream>
-#include "RPN.h"
+#include <string.h>
 #include <ctype.h>
-#include "euclid.h"
+#include "polynom.h"
 using namespace std;
 
-int ftn_exist(const char* input, char prev) {
-	if (op_priority(prev) == 2 || op_priority(prev) == 3 || prev == '(' || prev == ',') {
-		++input;
-		if (*input == 'c') {
-			++input;
-			if (*input == 'd') return 1;
-			else if (*input == 'm') return 2;
-		}
+poly* poly_get_monomial(int coef, int exp) {
+	poly* temp = (poly*)malloc(sizeof(poly));
+	if (temp) {
+		temp->coef = coef;
+		temp->exp = exp;
+		temp->next = NULL;
 	}
-	return 0;
+	return temp;
 }
 
-int op_priority(char op) {
-	switch (op) {
-	case '(': return 1;
-	case '+': return 2;
-	case '-': return 2;
-	case '*': return 3;
-	case '/': return 3;
-	case 'g': return 4;
-	case 'l': return 4;
-	case ')': return 5;
-	case ',': return 6;
-	default:
-		break;
-	}
+void poly_push(poly** head, int coef, int exp) {
+	poly* temp = poly_get_monomial(coef, exp);
+	temp->next = *head;
+	*head = temp;
 }
 
-void rpn_add(rpn** head, int num, char symb) {
-	if (num || symb) {
-		if ((*head)->next == NULL && (*head)->num == 0 && (*head)->symb == 0) {
-			(*head)->num = num;
-			(*head)->symb = symb;
+void poly_monom_print(int coeff, int exp) {
+	if (exp) {
+		if (coeff == 1) cout << 'x';
+		else if (coeff == (-1)) cout << '-' << 'x';
+		else cout << coeff << 'x';
+		if (exp != 1) cout << '^' << exp;
+	}
+	else cout << coeff;
+}
+
+void poly_print(poly* head) {
+	if (!(head)) {
+		cout << '0';
+		return;
+	}
+
+	if (!(head->coef)) {
+		if (!(head->next)) {
+			cout << '0' << '\n';
 			return;
 		}
-		rpn* new_elem = (rpn*)malloc(sizeof(rpn));
-		new_elem->num = num;
-		new_elem->symb = symb;
-		new_elem->next = NULL;
-		rpn* cur = *head;
-		while (cur->next) cur = cur->next;
-		cur->next = new_elem;
-		return;
+		head = head->next;
 	}
-	return;
-}
+	poly_monom_print(head->coef, head->exp);
+	head = head->next;
 
-void ops_add(ops** head, rpn** rhead, char op) {
-	if (!(*head)) {
-		(*head) = (ops*)malloc(sizeof(ops));
-		(*head)->op = op;
-		(*head)->next = NULL;
-		return;
-	}
-	if ((*head)->next == NULL && (*head)->op == 0) {
-		(*head)->op = op;
-		(*head)->next = NULL;
-		return;
-	}
-
-	ops* new_elem = (ops*)malloc(sizeof(ops));
-	new_elem->op = op;
-	if (op_priority(op) > op_priority((*head)->op) || op == '(') {
-		if (op_priority(op) > 4) {
-			while ((*head) && (*head)->op != '(') {
-				rpn_add(rhead, 0, (*head)->op);
-				ops_pop(head);
-			}
-			if (op == ')') {
-				ops_pop(head);
-				if (*head && (((*head)->op == 'g') || ((*head)->op == 'l'))) {
-					rpn_add(rhead, 0, (*head)->op);
-					ops_pop(head);
-				}
-			}
-		}
-		else {
-			new_elem->next = (*head);
-			(*head) = new_elem;
-		}
-		return;
-	}
-	while ((*head) && op_priority(op) <= op_priority((*head)->op)) {
-		rpn_add(rhead, 0, (*head)->op);
-		ops_pop(head);
-	}
-	if (!(*head)) {
-		(*head) = (ops*)realloc(*head, sizeof(ops));
-		(*head)->op = op;
-		(*head)->next = NULL;
-		return;
-	}
-	if (new_elem->op != ')') {
-		new_elem->next = (*head);
-		(*head) = new_elem;
-	}
-	return;
-}
-
-char rpn_pop(rpn** head) {
-	char symb = (*head)->symb;
-	(*head) = (*head)->next;
-	return symb;
-}
-
-char ops_pop(ops** head) {
-	if (*head) {
-		char symb = (*head)->op;
-		(*head) = (*head)->next;
-		return symb;
-	}
-}
-
-rpn* rpn_get(const char* input, int* err_ind) {
-	rpn* res = (rpn*)malloc(sizeof(rpn));
-	ops* head = (ops*)malloc(sizeof(ops));
-	res->num = 0;
-	res->symb = 0;
-	res->next = NULL;
-	head->op = 0;
-	head->next = NULL;
-
-	char prev = 0;
-	int sign = 0;
-	int ind = 0;
-	int opn_bt = 0;
-	int cls_bt = 0;
-
-	while (*input != '\n' && !(*err_ind)) {
-		if (isdigit(*input)) {
-			if (op_priority(prev) != 4 || prev != ')') {
-				char* endp = NULL;
-				int temp_num = strtol(input, &endp, 10);
-				rpn_add(&res, temp_num, 0);
-				ind += endp - input;
-				input = endp;
-				prev = *(input - 1);
-			}
-			else *err_ind = ind;
-		}
-		else if (op_priority(*input) == 2) {
-			if ((*input) == '-') ++sign;
-			++input;
-			++ind;
-			while (op_priority(*input) == 2) {
-				if ((*input) == '-') ++sign;
-				++input;
-				++ind;
-			}
-			if (sign & 1) ops_add(&head, &res, '-');
-			else ops_add(&head, &res, '+');
-			prev = *(input - 1);
-			sign = 0;
-		}
-		else if (*input == '*') {
-			if (isdigit(prev) || prev == ')') {
-				ops_add(&head, &res, *input);
-				prev = *input;
-				++input;
-				++ind;
-			}
-			else *err_ind = ind;
-		}
-		else if (*input == '/') {
-			if (isdigit(prev) || prev == ')') ops_add(&head, &res, *input);
-			else *err_ind = ind;
-			prev = *input;
-			++input;
-			++ind;
-		}
-		else if (*input == 'g' || *input == 'l') {
-			if (ftn_exist(input, prev) == 1) ops_add(&head, &res, 'g');
-			else ops_add(&head, &res, 'l');
-			prev = *input;
-			input += 3;
-			ind += 3;
-		}
-		else if (*input == '(') {
-			ops_add(&head, &res, '(');
-			++opn_bt;
-			prev = *input;
-			++input;
-			++ind;
-		}
-		else if (*input == ')') {
-			if ((1 < op_priority(prev) < 6) || isdigit(*input)) {
-				ops_add(&head, &res, *input);
-				++cls_bt;
-				if (cls_bt > opn_bt) *err_ind = ind;
-				prev = *input;
-				++input;
-				++ind;
-			}
-			else *err_ind = ind;
-		}
-		else if (*input == ',') {
-			if (isdigit(prev)) ops_add(&head, &res, *input);
-			else *err_ind = ind;
-			prev = *input;
-			++input;
-			++ind;
-		}
-		else *err_ind = ind;
-	}
-	ops_add(&head, &res, '+');
-	return res;
-}
-
-void rpn_print(rpn* head) {
 	while (head) {
-		if (head->symb) cout << head->symb << ' ';
-		else if (head->num) cout << head->num << ' ';
+		if (head->coef > 0) cout << '+';
+		poly_monom_print(head->coef, head->exp);
 		head = head->next;
 	}
 	cout << '\n';
 	return;
 }
 
-int rpn_count(rpn* head) {
-	int res = 0;
-
-	ct* chead = (ct*)malloc(sizeof(ct));
-	chead->num = head->num;
-	chead->next = NULL;
-	head = head->next;
-
-	ct* new_elem = (ct*)malloc(sizeof(ct));
-	while (head) {
-		if (head->num) {
-			new_elem->num = head->num;
-			new_elem->next = chead;
-			*chead = new_elem;
+void poly_add(poly** head, int coef, int exp) {
+	if (coef == 0) return;
+	poly* curr = *head;
+	poly* prev = (poly*)malloc(sizeof(poly));
+	prev->coef = 0;
+	while (curr->next) {
+		if (curr->exp > exp) {
+			prev = curr;
+			curr = curr->next;
+		}
+		else if (curr->exp == exp) {
+			curr->coef += coef;
+			if (!(curr->coef)) prev->next = curr->next;
+			return;
 		}
 		else {
-			int num1; int num2;
-			num1 = chead->num;
-			chead = chead->next;
-			num2 = chead->num;
-			chead = chead->next;
-			if (head->symb == '+') {
-				res += num1 + num2;
-				new_elem->num = num1 + num2;
-				new_elem->next = chead;
-				chead = new_elem;
-			}
-			else if (head->symb == '-') {
-				res += num2 - num1;
-				new_elem->num = num2 - num1;
-				new_elem->next = chead;
-				chead = new_elem;
-			}
-			else if (head->symb == '*') {
-				res += num1 * num2;
-				new_elem->num = num1 * num2;
-				new_elem->next = chead;
-				chead = new_elem;
-			}
-			else if (head->symb == '/') {
-				res += num2 / num1;
-				new_elem->num = num2 / num1;
-				new_elem->next = chead;
-				chead = new_elem;
-			}
-			else if (head->symb == 'g') {
-				res += gcd(num1, num2);
-				new_elem->num = gcd(num1, num2);
-				new_elem->next = chead;
-				chead = new_elem;
-			}
-			else if (head->symb == 'l') {
-				res += lcm(num1, num2);
-				new_elem->num = lcm(num1, num2);
-				new_elem->next = chead;
-				chead = new_elem;
-			}
+			poly* temp = poly_get_monomial(coef, exp);
+			temp->next = curr;
+			if (prev->coef) prev->next = temp;
+			else (*head) = temp;
+			return;
 		}
-		head = head->next;
+	}
+	if (curr->exp > exp) {
+		poly* temp = poly_get_monomial(coef, exp);
+		curr->next = temp;
+	}
+	else if (curr->exp == exp) {
+		curr->coef += coef;
+		if (!(curr->coef)) prev->next = curr->next;
+	}
+	else {
+		if (curr->coef == 0) {
+			(*head)->coef = coef;
+			(*head)->exp = exp;
+		}
+		else {
+			poly* temp = poly_get_monomial(coef, exp);
+			if (prev->coef) {
+				temp->next = curr;
+				prev->next = temp;
+			}
+			else poly_push(head, coef, exp);
+		}
+	}
+	return;
+}
+
+poly* poly_get(const char* str) {
+	poly* result = poly_get_monomial(0, 0);
+	char prev = NULL;
+	int ind = 0;
+	int temp_num = 1;
+	bool x_check = 0;
+	int sgn = 0;
+	int coef_temp = 0;
+	int exp_temp = 0;
+
+	while (*str == '-' || *str == '+') {
+		if (!(prev) || prev == '+' || prev == '-') {
+			if (*str == '-') ++sgn;
+			prev = *str;
+			++ind;
+			++str;
+		}
+		else throw ind;
+	}
+
+	while (*str != '\n') {
+		if (*str == 'x') {
+			if (!(prev) || (isdigit(prev)) || prev == '+' || prev == '-') {
+				if (sgn & 1) coef_temp = -temp_num;
+				else coef_temp = temp_num;
+				x_check = 1;
+				sgn = 0;
+				temp_num = 1;
+				prev = *str;
+				++str;
+				++ind;
+			} 
+			else throw ind;
+		}
+		else if (*str == '+' || *str == '-') {
+			if (prev == '+' || prev == '-' || (isdigit(prev)) || prev == 'x') {
+				if (x_check) exp_temp = temp_num;
+				else {
+					if (sgn & 1) coef_temp = -temp_num;
+					else coef_temp = temp_num;
+					exp_temp = 0;
+					sgn = 0;
+				}
+				poly_add(&result, coef_temp, exp_temp);
+				x_check = 0;
+				temp_num = 1;
+				while (*str == '-' || *str == '+') {
+					if (*str == '-') ++sgn;
+					prev = *str;
+					++str;
+					++ind;
+				}
+			}
+			else throw ind;
+		}
+		else if (*str == '^') {
+			if (prev == 'x') {
+				prev = *str;
+				++str;
+				++ind;
+			}
+			else throw ind;
+		}
+		else if (isdigit(*str)) {
+			if (prev == '+' || prev == '-' || prev == '^' || !(prev)) {
+				char* endp = NULL;
+				temp_num = strtol(str, &endp, 10);
+				ind += endp - str;
+				str = endp;
+				prev = *(str - 1);
+			}
+			else throw ind;
+		}
+		else throw ind;
+	}
+	if (!ind) {
+		result = NULL;
+		return result;
+	}
+	else {
+		if (x_check) exp_temp = temp_num;
+		else {
+			if (sgn & 1) coef_temp = -temp_num;
+			else coef_temp = temp_num;
+			exp_temp = 0;
+		}
+		poly_add(&result, coef_temp, exp_temp);
+	}
+	return result;
+}
+
+poly* poly_sum(poly* first, poly* sec) {
+	while (sec) {
+		poly_add(&first, sec->coef, sec->exp);
+		sec = sec->next;
+	}
+	return first;
+}
+
+poly* poly_mult(poly* first, poly* sec) {
+	poly* res = poly_get_monomial(0, 0);
+	poly* cur;
+	while (first) {
+		poly* cur = sec;
+		while (cur) {
+			poly_add(&res, first->coef * cur->coef, first->exp + cur->exp);
+			cur = cur->next;
+		}
+		first = first->next;
 	}
 	return res;
 }
