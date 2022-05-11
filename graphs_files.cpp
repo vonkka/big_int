937be3aa9792b7241a5c55db1ed72e7c783568e4
vonkka <96284529+vonkka@users.noreply.github.com>
2022-05-11 22:45:21 +0300

    Update graphs_files.cpp

--- graphs_files.cpp
+++ graphs_files.cpp
@@ -1,3 +1,19 @@
+cout << "Enter the graph size: ";
+    int size;
+    cin >> size;
+    graph* check_gr = graph_create(size);
+    for (int i = 0; i < size; ++i) {
+        int input = 1; int len = 0;
+        int* adj = str_to_mas_int(&input, &len);
+        for (int k = 0; k < len; ++k) {
+            if (i != adj[k]) add_adj(&check_gr->adj_list[i].head, adj[k]);
+        }
+    }
+    if (check_gr) {
+        graph_print(check_gr);
+        graph_out(check_gr, "C:/Users/1/Desktop/Programming/graph_out.txt");
+    }
+
 int graph_in(const char* filename) {
 	FILE* myfile;
 	errno_t err;
@@ -24,6 +40,29 @@ int graph_in(const char* filename) {
 			graph_print(g);
 		}
 		fclose(myfile);
+		return 1;
+	}
+	return 0;
+}
+
+int graph_out(graph* g, const char* filename) {
+	FILE* myfile;
+	errno_t err;
+	err = fopen_s(&myfile, filename, "w");
+	if (!err) {
+		fprintf(myfile, "%d\n", g->count);
+		for (int i = 0; i < g->count; ++i) {
+			fprintf(myfile, "%d: ", i);
+			grph_node* node_ptr = g->adj_list[i].head;
+			if (node_ptr) {
+				while (node_ptr->next) {
+					fprintf(myfile, "%d, ", node_ptr->num);
+					node_ptr = node_ptr->next;
+				}
+				fprintf(myfile, "%d", node_ptr->num);
+			}
+			fprintf(myfile, "%c", '\n');
+		}
 		return 1;
 	}
 	return 0;
